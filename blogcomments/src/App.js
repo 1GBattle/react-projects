0f97543dd65d558e06e03fd13ed002853555960f
Gary Battle <garybattle321@gmail.com>
2021-07-26 22:19:35 -0500

    Blog comments folder added to project

--- blogcomments/src/App.js
+++ blogcomments/src/App.js
@@ -1,27 +1,48 @@
 import faker from 'faker'
+import ApprovalCard from './components/Approval-Card'
 import Comments from './components/Comments'
 
 const App = () => {
   return (
     <>
-      <Comments
-        image={faker.image.avatar()}
-        userName={faker.internet.userName()}
-        comment={faker.lorem.sentences(Math.floor(Math.random() * 10) + 1)}
-        timePosted={faker.time.recent()}
-      />
-      <Comments
-        image={faker.image.avatar()}
-        userName={faker.internet.userName()}
-        comment={faker.lorem.sentences(2)}
-        timePosted={faker.time.recent()}
-      />
-      <Comments
-        image={faker.image.avatar()}
-        userName={faker.internet.userName()}
-        comment={faker.lorem.sentences(2)}
-        timePosted={faker.time.recent()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+        </div>
+        Are you sure
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comments
+          image={faker.image.avatar()}
+          userName={faker.internet.userName()}
+          comment={faker.lorem.sentences(2)}
+          timePosted={faker.time.recent()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comments
+          image={faker.image.avatar()}
+          userName={faker.internet.userName()}
+          comment={faker.lorem.sentences(2)}
+          timePosted={faker.time.recent()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comments
+          image={faker.image.avatar()}
+          userName={faker.internet.userName()}
+          comment={faker.lorem.sentences(2)}
+          timePosted={faker.time.recent()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comments
+          image={faker.image.avatar()}
+          userName={faker.internet.userName()}
+          comment={faker.lorem.sentences(2)}
+          timePosted={faker.time.recent()}
+        />
+      </ApprovalCard>
     </>
   )
 }
