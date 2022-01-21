3e7fe4747cff0ff8d24fd0e65abc3c8fb8be7a99
thirdxcoast <garybattle321@gmail.com>
2022-01-21 12:27:42 -0600

    added delete todo functionality

--- todo-app/src/components/TodoFilterInput.js
+++ todo-app/src/components/TodoFilterInput.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 import '../styles/TodoFilterInput.css'
 
 const TodoFilterInput = ({ todos }) => {
+  const filterTodos = (searchTerm) => {
+    todos.map((todo) => todo).filter((todo) => todo.title.includes(searchTerm))
+  }
+
   return (
     <div className='filter-input-container'>
       <input className='filter-input' placeholder='Find Todo' />
