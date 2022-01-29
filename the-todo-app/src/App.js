4bf7cdab383a27b6dc186e3ff1524767eced1082
thirdxcoast <garybattle321@gmail.com>
2022-01-29 02:49:12 -0600

    Routing update

--- the-todo-app/src/App.js
+++ the-todo-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Router, Route, Routes } from 'react-router-dom'
 import CreateNewTodo from './Components/CreateNewTodo'
 import DashBoard from './Components/Dashboard'
 import EditTodo from './Components/EditTodo'
@@ -8,7 +8,7 @@ import Header from './Components/Header'
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <Header />
 
         <Routes>
@@ -16,7 +16,7 @@ function App() {
           <Route exact path='/create' component={CreateNewTodo} />
           <Route exact path='/edit/:id' component={EditTodo} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
