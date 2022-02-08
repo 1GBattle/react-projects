9b5b6dfd443a5e23e7cdce65722b4d9f86f2c3d9
thirdxcoast <garybattle321@gmail.com>
2022-02-08 13:59:05 -0600

    initial commit

--- pic-search/src/Components/ImageList.js
+++ pic-search/src/Components/ImageList.js
@@ -5,10 +5,10 @@ import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <ImageCard key={image.id} image={image} alt='' />
+    return <ImageCard key={image.id} image={image} alt="" />
   })
 
-  return <div className='image-list'>{images}</div>
+  return <div className="image-list">{images}</div>
 }
 
 export default ImageList
