f6655c3b8de26ded260fdf07bb447c75d9bf734e
thirdxcoast <garybattle321@gmail.com>
2022-02-09 01:19:13 -0600

    Adjustment to active books grid

--- book-keeper-app/src/Components/BookList.jsx
+++ book-keeper-app/src/Components/BookList.jsx
@@ -11,13 +11,14 @@ const BookList = ({ listTitle, books }) => {
   return (
     <div className="centered-page book-list-container">
       <h1 className="book-list-title">{listTitle}</h1>
+      <div className="books-container">
+        {books.map((book) => {
+          const progressBarRatio = Math.floor(
+            (book.completedPages / book.pages) * 100
+          )
 
-      {books.map((book) => {
-        const progressBarRatio = book.pages / book.completedPages + 60
-
-        return (
-          <div className="books-container" key={book.id}>
-            <div className="book-container">
+          return (
+            <div className="book-container" key={book.id}>
               <img
                 className="book-img"
                 src="https://books.google.com/books/content?id=m3GzoAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
@@ -31,8 +32,9 @@ const BookList = ({ listTitle, books }) => {
                   </div>
                 </div>
 
-                <p className="book-author">Written By {book.author}</p>
-                <p className="book-pages">{book.pages} Pages</p>
+                <h3 className="book-title">{book.bookName}</h3>
+                <p className="book-author">Written By: {book.author}</p>
+                <p className="book-pages">Book Length: {book.pages} Pages</p>
                 <p className="book-genre">Genre: {book.genre}</p>
               </div>
 
@@ -40,18 +42,18 @@ const BookList = ({ listTitle, books }) => {
                 <button className="btn add-completed-pages-btn">
                   Compete Pages
                 </button>
-                <button
-                  className="btn delete-btn"
-                  onClick={() => dispatch(removeBook(book.id))}
-                >
-                  Remove Book
-                </button>
+                <button className="btn edit-book-btn">Edit Book Details</button>
               </div>
-              <button className="btn edit-book-btn">Edit Book Details</button>
+              <button
+                className="btn delete-btn"
+                onClick={() => dispatch(removeBook(book.id))}
+              >
+                Remove Book
+              </button>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </div>
   )
 }
